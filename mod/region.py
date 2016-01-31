ff74d3ca95e76031ed16e26edf97ffe387f1e001
Roger <hiroger@qq.com>
2016-01-31 11:21:37 +0800

    add methods of region

--- mod/region.py
+++ mod/region.py
@@ -1,3 +1,6 @@
+import numpy as np
+import numpy.random as random
+
 class population(object):
     """population"""
     def __init__(self, population, icrrate):
@@ -19,11 +22,48 @@ class infras(object):
 
 class Region(object):
     """Region"""
-    def __init__(self, pop, capacity, water, ifrstrc):
+    def __init__(self, pop, capacity, water, ifrstrc, dt):
         self.pop = pop
         self.capacity = capacity
         self.water = water
         self.ifrstrc = ifrstrc
+        self.dt = dt
 
     def __repr__(self):
         return '%s\n%s'%(self.water,self.pop)
+
+    def next_timestep(self):
+        return self.mu()*self.dt+self.sigma()
+
+    def mu(self):
+        return (self.water-self.cost()+self.produce())/self.water
+
+    def sigma(self):
+        return self.uncrt_produce()-self.uncrt_cost()+np.sqrt(dt)*random.randn()
+
+    def uncrt_cost(self):
+        return
+
+    def uncrt_produce(self):
+        return
+
+    def cost(self):
+        return self.agriculture()+self.industrial()+self.resident()
+
+    def produce(self):
+        return self.waterrecycle()
+
+    def waterrecycle(self):
+        return
+
+    def agriculture(self):
+        return
+
+    def industrial(self):
+        pass
+
+    def resident(self):
+        pass
+
+
+Region()
