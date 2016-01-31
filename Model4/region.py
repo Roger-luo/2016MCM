62303f320a3372f31fba0023bbdce5aa6e569c71
Roger-luo <hiroger@qq.com>
2016-02-01 04:27:17 +0800

    s

--- Model4/region.py
+++ Model4/region.py
@@ -1,3 +1,6 @@
+import numpy as np
+from numpy.random import randn
+
 class Region(object):
     """docstring for Region"""
     def __init__(self, pop, capacity, water, ifrstrc, pGDP, year, dt):
@@ -12,11 +15,36 @@ class Region(object):
     def mu(self):
         return 0
 
+    def sigma(self):
+        return 0
+
     def nextstep(self):
-        return self.water_supply
+        self.water.water_supply = self.water.water_supply+self.water.water_supply*(self.mu()*self.dt+self.sigma()*np.sqrt(self.dt)*randn())
 
     def nextyear(self):
-        for 
+        for i in np.linspace(0,1,100):
+            self.nextstep()
+
+class infras(object):
+    """object for infrastructure"""
+    def __init__(self, dam, recycle):
+        self.dam = dam
+        self.recycle = recycle
+
+    def average(self):
+        return self.dam+self.recycle
+
+    def variance(self):
+        return 0
+
+    def recycle(self):
+        return 0
+
+    def desalt(self):
+        return 0
+
+    def produce(self):
+        return self.recycle()+self.desalt()
 
 class water(object):
            """docstring for water"""
