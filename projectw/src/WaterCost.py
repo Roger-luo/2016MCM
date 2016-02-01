3257514d0844bb92fe08dadac9d1a278ff6d6f9a
Roger-luo <hiroger@qq.com>
2016-02-01 19:52:10 +0800

    add correlation

--- projectw/src/WaterCost.py
+++ projectw/src/WaterCost.py
@@ -16,6 +16,11 @@ class IndustWater(object):
         self.gcoeff = self.para(self.PCGDP.data)
         self.ecoeff = self.para(self.Elec.data)
         self.scoeff = self.para(self.Steel.data)
+        pass
+        self.pvar = self.variance(self.Pop,self.pcoeff)
+        self.gvar = self.variance(self.PCGDP,self.gcoeff)
+        self.evar = self.variance(self.Elec,self.ecoeff)
+        self.svar = self.variance(self.Steel,self.scoeff)
         
     def Gaussian(self, x,p):
         return p[0]*np.exp(-p[1]*(x-p[2])**2)
@@ -27,6 +32,9 @@ class IndustWater(object):
         x = [datax[t] for t in years]
         y = [self.WaterUse[t] for t in years]
         return leastsq(self.err,[y[0],1e-8,x[y.index(np.max(y))]],args=(x,y))[0]
+
+    def variance(self, ob, coeff):
+        return np.mean([(ob.data[i]-self.Gaussian(i,coeff))**2 for i in years])
 
     def __call__(self, time):
         # if time>2015:
@@ -40,7 +48,7 @@ class IndustWater(object):
         ele = (self.Gaussian(self.Elec(time),self.ecoeff)/self.Gaussian(self.Elec(years[-1]),self.ecoeff))
         spr = (self.Gaussian(self.Steel(time),self.scoeff)/self.Gaussian(self.Steel(years[-1]),self.scoeff))
         # print((pop/4+pcg/4+ele/4+spr/4))
-        return self.WaterUse[years[-1]]*(pop/4+pcg/4+ele/4+spr/4)
+        return self.WaterUse[years[-1]]*(0.584*pop+0.565*pcg+0.577*ele+0.577*spr)/2.303
 
 class AgriWater(object):
     """docstring for AgriWater"""
@@ -68,7 +76,7 @@ class AgriWater(object):
         pop = (self.Expo(self.Pop(time),self.pcoeff)/self.Expo(self.Pop(years[-1]),self.pcoeff))
         pcg = (self.Expo(self.PCGDP(time),self.gcoeff)/self.Expo(self.PCGDP(years[-1]),self.gcoeff))
         ira = (self.Expo(self.IrrArea(time),self.icoeff)/self.Expo(self.IrrArea(years[-1]),self.icoeff))
-        return self.WaterUse[years[-1]]*(pop/3+pcg/3+ira/3)
+        return self.WaterUse[years[-1]]*(0.878*pop+0.882*pcg+0.879*ira)/2.639
 
 class ResWater(object):
     """docstring for ResWater"""
@@ -93,4 +101,4 @@ class ResWater(object):
     def __call__(self,time):
         pop = (self.Expo(self.Pop(time),self.pcoeff)/self.Expo(self.Pop(years[-1]),self.pcoeff))
         pcg = (self.Expo(self.PCGDP(time),self.gcoeff)/self.Expo(self.PCGDP(years[-1]),self.gcoeff))
-        return self.WaterUse[years[-1]]*(pop/2+pcg/2)
+        return self.WaterUse[years[-1]]*(0.808*pop+0.796*pcg)/1.604
