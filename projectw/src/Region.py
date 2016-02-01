789d9250ab93b13349c5327bbc0ad0196e2433ff
Roger-luo <hiroger@qq.com>
2016-02-01 20:03:49 +0800

    add pics

--- projectw/src/Region.py
+++ projectw/src/Region.py
@@ -21,17 +21,35 @@ def AverSupply(time):
     else:
         return (Wr(time)+Wi(time)+Wa(time))/pop(time)
 
-# x = range(years[0],years[-1]+15)
-# # y = AverSupply(x)
-
-
-# print(AverSupply(years[-1]+15))
-
-
 
 plt.figure()
-plt.xlabel("years")
-plt.scatter(years,[Data.WaterUseTotal[t]/Data.Population[t] for t in years],marker="^",s=100)
-plt.plot(range(2004,2030),[AverSupply(i) for i in range(2004,2030)],'b-')
+plt.xlabel("years /yr")
+plt.ylabel("Ability /per person per yr 100m cubic meters")
+plt.scatter(years,[Data.WaterUseTotal[t]/Data.Population[t] for t in years],marker="^",s=100,label='raw data')
+plt.plot(range(2004,2030),[AverSupply(i) for i in range(2004,2030)],'b-',label="prediction")
+plt.legend(loc=0)
 plt.show()
-# plt.close()
+
+plt.figure()
+plt.xlabel("years /yr")
+plt.ylabel("Industrial Water Cost /100m cubic meters per yr")
+plt.scatter(years,[Data.WaterUseIndustry[t] for t in years],marker="^",s=100,label='raw data')
+plt.plot(range(2004,2030),[Wi(i) for i in range(2004,2030)],'b-',label="prediction")
+plt.legend(loc=0)
+plt.show()
+
+plt.figure()
+plt.xlabel("years /yr")
+plt.ylabel("Residential Water Cost /100m cubic meters per yr")
+plt.scatter(years,[Data.WaterUseResidential[t] for t in years],marker="^",s=100,label='raw data')
+plt.plot(range(2004,2030),[Wr(i) for i in range(2004,2030)],'b-',label="prediction")
+plt.legend(loc=0)
+plt.show()
+
+plt.figure()
+plt.xlabel("years /yr")
+plt.ylabel("Agricultural Water Cost /100m cubic meters per yr")
+plt.scatter(years,[Data.WaterUseAgriculture[t] for t in years],marker="^",s=100,label='raw data')
+plt.plot(range(2004,2030),[Wa(i) for i in range(2004,2030)],'b-',label="prediction")
+plt.legend(loc=0)
+plt.show()
